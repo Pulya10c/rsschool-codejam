50084b9eca8eb48ee61d7a10b3a7b6a8a03d8408
Pulya10c <sergienya.oleg@yandex.ru>
2018-10-28 17:13:34 +0300

    fix: fixed error, edited code text

--- make/make.js
+++ make/make.js
@@ -1,16 +1,11 @@
-function sum(a, b) {
-    return a + b;
-}
-
-function make() {
-    let current = [arguments[0]];
-    function f(...arg) {
-    if ( typeof arg[0] !== 'function') {
-        current.push(...arg);
-        return f;
+module.exports = function make(...arr) {
+  const current = [arr[0]];
+  function f(...arg) {
+    if (typeof arg[0] !== 'function') {
+      current.push(...arg);
+      return f;
     }
-      return current.reduce(sum);
-}
-return f;
-  };
-
+    return current.reduce(arg[0]);
+  }
+  return f;
+};
